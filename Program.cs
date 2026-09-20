5b9e6900bf4c79164acc93081c68026b38c971cc
ym9079389-alt <ym9079389@gmail.com>
2026-09-20 14:34:36 +0300

    Q1 => b

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@
             // Abstraction concept of hiding complex implementation details and showing only the finl result of an object to the user.
             #endregion
 
+            #region b
+            //b)  Why is abstraction considered one of the four pillars of OOP ?
+            //Enhances Maintainability & Security: Protects internal code from changes.
+            //Enables Reusability: Provides reusable blueprints across the project.
+            #endregion
 
             #endregion
         }
