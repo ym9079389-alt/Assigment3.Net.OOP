9fd7e6869271712fc728f9039dae95b4f04481fa
ym9079389-alt <ym9079389@gmail.com>
2026-09-20 14:40:53 +0300

    Q2 => a

--- Program.cs
+++ Program.cs
@@ -18,6 +18,16 @@
             #endregion
 
             #endregion
+
+            #region Q2
+
+            #region a
+
+            #endregion
+            //a)  What is the difference between an Abstract Class and an Interface ?
+            //Interface: cannot use constractor.
+            //Abstract: can use constractor and implementation the proparety in abstract.
+            #endregion
         }
     }
 }
